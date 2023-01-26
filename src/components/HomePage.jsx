f4169d5f2e6bbceba9c8c9729665450887462b6d
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2023-01-27 00:06:39 +0300

    fix not used vars

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import avatar1 from "../images/avatars/AVATAR-1.png";
 import avatar2 from "../images/avatars/AVATAR-2.png";
 import avatar3 from "../images/avatars/AVATAR-3.png";
-import avatar4 from "../images/avatars/AVATAR-4.png";
-import avatar5 from "../images/avatars/AVATAR-5.png";
-import avatar6 from "../images/avatars/AVATAR-6.png";
-import avatar7 from "../images/avatars/AVATAR-7.png";
-import avatar8 from "../images/avatars/AVATAR-8.png";
-import avatar9 from "../images/avatars/AVATAR-9.png";
+// import avatar4 from "../images/avatars/AVATAR-4.png";
+// import avatar5 from "../images/avatars/AVATAR-5.png";
+// import avatar6 from "../images/avatars/AVATAR-6.png";
+// import avatar7 from "../images/avatars/AVATAR-7.png";
+// import avatar8 from "../images/avatars/AVATAR-8.png";
+// import avatar9 from "../images/avatars/AVATAR-9.png";
 import { useDispatch, useSelector } from "react-redux";
 import {
   setAvatar,
